d817ef31d9bf6e921983a5fc3b5eee5526f0383e
Pedro de Oliveira <pedroarthuralvesdeoliveira1@gmail.com>
2025-11-14 15:25:13 -0300

    Hotfix: remove trailing comma from

--- cancel_appointment.py
+++ cancel_appointment.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException, 
+from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
