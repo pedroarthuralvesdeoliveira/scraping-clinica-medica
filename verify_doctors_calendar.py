96aa603b8d69dadb864e94185d97669915e799d9
Pedro de Oliveira <pedroarthuralvesdeoliveira1@gmail.com>
2025-11-21 15:14:18 -0300

    Add headless mode again

--- verify_doctors_calendar.py
+++ verify_doctors_calendar.py
@@ -25,11 +25,10 @@ def verify_doctors_calendar(
 
     options = Options()
     options.add_argument("--lang=pt-BR")
-    # options.add_argument("--headless=new")
-    # options.add_argument("--no-sandbox") # Necessário para rodar como root/em containers
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox") # Necessário para rodar como root/em containers
     options.add_argument("--disable-dev-shm-usage") # Necessário para alguns ambientes Linux
     options.add_argument("--disable-gpu")
-    # options.add_argument("--window-size=1920,1080")
     
     prefs = {
          "profile.default_content_setting_values.notifications": 0
